cdf44284a459f386afb012e60f8ef0ff9ce7a049
roberto.momberger <roberto88momberger@gmail.com>
2021-02-09 02:09:57 -0300

    config for heroku

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + "/index.html"));
+app.use(express.static(__dirname + "/public"));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
