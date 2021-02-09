ee895ac35f4fe6570625eb7950e6765f8e8ef718
roberto.momberger <roberto88momberger@gmail.com>
2021-02-09 01:50:53 -0300

    config for heroku

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + "/public"));
-app.listen(port);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
